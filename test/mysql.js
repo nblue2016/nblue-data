9e204633e5d3b2bf85d2fb4adb35593b4e4a9291
Yifeng Jiang <yifeng.jiang@mkcorp.com>
2016-05-16 18:03:15 +0800

    Updated

---
+++ test/mysql.js
@@ -0,0 +1,25 @@
+const Data = require('../../').data
+
+const mysqlConns = new Data.MySQLDBConnections()
+const connectionString = 'mysql://elliemae_ps:elliemae-sh-13@10.10.73.209/db-ps?pool=true'
+const cs = mysqlConns.parseConnectionString(connectionString, { "typeCast": true, "stringifyObjects": false })
+// console.log(cs)
+
+mysqlConns.create('test1', connectionString, {})
+
+const mysqlConn = mysqlConns.get('test1')
+
+mysqlConn.createConnection()
+  .then(conn => mysqlConn.query(conn, 'SELECT col1 FROM table1'))
+  .then( data => console.log('a:' + JSON.stringify(mysqlConn.repackageData(data))))
+  .finally(() => mysqlConn.end())
+
+
+mysqlConn.execute('SELECT col1 FROM table1')
+  .then( data => console.log('b:' + JSON.stringify(mysqlConn.repackageData(data))))
+  .catch( err => { console.log(err) })
+
+mysqlConn.execute('select max(col1) + 1 as a from table1')
+ .then( data => console.log(data) )
+ .catch( err => console.log(err) )
+ .finally( () => mysqlConn.end() )
---
+++ test/test_schema.js
@@ -0,0 +1,20 @@
+
+const path = require('path')
+const data = require('../../').data
+
+// const Cache = data.SchemaCache
+
+const cache = data.SchemaCache.create()
+
+console.log(cache)
+
+let file = undefined
+
+file = path.join(__dirname, '../data/schemas/blog.json')
+cache.add(file)
+
+file = path.join(__dirname, '../data/schemas/blog.js')
+cache.add(file)
+
+const schemas = cache.getSchemas('mongo')
+console.log(schemas)
