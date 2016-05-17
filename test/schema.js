68c6a7364950338d4c97e66d11c6a5102ced80a6
Yifeng Jiang <yifeng.jiang@mkcorp.com>
2016-05-17 17:43:00 +0800

    Update

--- test/schema.js
+++ test/schema.js
@@ -1,20 +1,24 @@
 
 const path = require('path')
-const data = require('../../').data
+const data = require('../lib')
 
 // const Cache = data.SchemaCache
 
 const cache = data.SchemaCache.create()
 
-console.log(cache)
+// console.log(cache)
 
 let file = undefined
 
-file = path.join(__dirname, '../data/schemas/blog.json')
+file = path.join(__dirname, './schemas/blog.json')
 cache.add(file)
 
-file = path.join(__dirname, '../data/schemas/blog.js')
+file = path.join(__dirname, './schemas/blog._js')
 cache.add(file)
 
 const schemas = cache.getSchemas('mongo')
-console.log(schemas)
+if (schemas.has('post')) {
+  console.log(schemas.get('post'))
+}
+// console.log(Object.keys(schemas.toObject()))
+// console.log(schemas.values())
