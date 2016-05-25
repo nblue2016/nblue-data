f2a941e8a2eab94635b74720a0aaa2a58ebdfe6f
Bruce Jiang <nblue2016@163.com>
2016-05-25 18:13:06 +0800

    fixed

--- test/test.js
+++ test/test.js
@@ -1,34 +1,3 @@
-const assert = require('assert')
-const fs = require('fs')
-const path = require('path')
+require('nblue-core')
 
-const dataLib = require('../lib')
-
-const config = global.config
-const MongoDbConnections = dataLib.MongoDbConnections
-const MongoDbAdapter = dataLib.MongoDbAdapter
-
-const conns = new MongoDbConnections()
-const files = ['/schemas/blog.json', '/schemas/northwind.json']
-
-files.
-  map((file) => path.join(__dirname, file)).
-  map((file) => JSON.parse(fs.readFileSync(file))).
-  forEach((schema) => conns.appendSchema(schema))
-
-conns.createByConfig('conn1', config)
-
-conns.on('open', () => {
-  const dbAdapter = new MongoDbAdapter(conns)
-
-  dbAdapter.create(
-    'post',
-    { title: 'good news' },
-    (err, data) => {
-      console.log(err)
-      if (err) console.log(err.message)
-      assert('1', 1, data)
-      console.log(data)
-    }
-  )
-})
+console.log('test')
