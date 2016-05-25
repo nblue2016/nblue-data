769d9d68d2e6431b22806b691b12fe8deddab2c8
Bruce Jiang <nblue2016@163.com>
2016-05-25 15:32:52 +0800

    Add comment

--- test/start.js
+++ test/start.js
@@ -6,6 +6,7 @@ const dataLib = require('../lib')
 const ConfigMap = global.ConfigMap
 const SchemaCache = dataLib.SchemaCache
 
+// parse schema files
 const cache = SchemaCache.create()
 
 const files = ['blog._js', 'blog.json', 'northwind.json']
@@ -14,21 +15,26 @@ files.
   map((file) => path.join(__dirname, 'schemas', file)).
   map((file) => cache.add(file))
 
+// only get schema of entities for mongo-db
 const schemas = cache.getSchemas('mongo')
 
+if (!global.schemas) global.schemas = schemas
+
+// parse configuration file
+const configFile = String.format('%s/config.yml', __dirname)
+const config = ConfigMap.
+  parseConfigSync(configFile, ['dev', 'debug'])
+
+if (!global.config) global.config = config
+
+// define test script files
 const testScripts = [
   './connections',
   './schema',
   './adapter'
 ]
 
-const configFile = String.format('%s/config.yml', __dirname)
-const config = ConfigMap.
-  parseConfigSync(configFile, ['dev', 'debug'])
-
-if (!global.config) global.config = config
-if (!global.schemas) global.schemas = schemas
-
+// start to test
 for (const script of testScripts) {
   if (script.startsWith('#')) continue
 
