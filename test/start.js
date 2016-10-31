279d702209138ab3d7ac0cc8dfdb1469eba1ab38
Bruce Jiang <nblue2016@163.com>
2016-10-31 17:39:49 +0800

    Upading

--- test/start.js
+++ test/start.js
@@ -1,54 +1,13 @@
-// const nblue = require('nblue')
-
-// const path = require('path')
-// const betch = nblue.betch
-
-// const ndata = require('../lib')
-
-// const ConfigMap = global.ConfigMap
-// const Schemas = nblueData.Schemas
-
 // define test script files
 const testScripts = [
-  '#./schema',
-  '#./connections',
+  './schema',
+  './connections',
   './adapter/crud',
   '#./adapter/default',
   '#./adapter/validator'
 ]
 
-/*
-const files = ['blog.json', 'blog.js', 'northwind.json'].
-  map(
-    (file) => path.join(__dirname, 'schemas', file)
-  )
-
-const configFile = String.format('%s/config.yml', __dirname)
-*/
-
 describe('init envirnment', () => {
-
-  /*
-  before('', (done) => {
-    const ctx = {}
-
-    betch({
-      cf: ConfigMap.parseConfigSync(
-            configFile,
-            ['dev', 'debug', 'qa']
-          ),
-      sc: Schemas.parse(files)
-    }, ctx).
-    then(() => {
-      global.config = ctx.cf
-      global.schemas = ctx.sc.Cache
-
-      done()
-    }).
-    catch((err) => done(err))
-  })
-  */
-
   it('exec scripts', () => {
     for (const script of testScripts) {
       if (script.startsWith('#')) continue
